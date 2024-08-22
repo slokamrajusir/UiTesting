2174e884b4ce82815c8af333c7a5e1c2acdf3d4c
rajagopal <rrgn1236@gmail.com>
2024-08-22 11:59:59 +0530

    Second file

---
+++ raja1.java
@@ -0,0 +1,11 @@
+public class b
+{
+	public static void main(String[] args)
+	{
+		String b[]={"hai","hello","Good morning","good Afternoon","Good Evening"};
+		for(String j : b)
+		{
+			System.out.print(b[j]);
+		}
+	}
+
