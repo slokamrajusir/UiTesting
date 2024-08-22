3baf0e09d122d9231a3640b6b4e81deea5f90816
rajagopal <rrgn1236@gmail.com>
2024-08-22 11:52:51 +0530

    hello

---
+++ raja.java
@@ -0,0 +1,13 @@
+Public class num
+{
+	public static void main(String[] args)
+	{
+		int a[]={1,2,3,4,5};
+		for(int i = 0; i< = a.length-1; i++)
+		{
+			System.out.println(a[i]);
+
+	         }
+	}
+
+}
